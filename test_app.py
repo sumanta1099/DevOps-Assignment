fba9024a4fe58499e81dbfec2b6fa8648be3b59e
sumanta <sumantasen22@gmail.com>
2024-04-29 03:28:20 +0530

    4th commit

--- test_app.py
+++ test_app.py
@@ -1,13 +1,15 @@
+# test_app.py
 import unittest
 from app import app
 
 class TestApp(unittest.TestCase):
-    def test_hello(self):
-        tester = app.test_client(self)
-        response = tester.get('/')
+    def setUp(self):
+        self.app = app.test_client()
+    
+    def test_hello_world(self):
+        response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello, World!')
 
 if __name__ == '__main__':
     unittest.main()
-    
